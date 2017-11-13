2b70c7779a8967572c3c9e603ff8f86c98a3db6f
john.bayly@tipstrade.net <john.bayly@tipstrade.net>
2017-11-13 17:15:01 +0000

    Initial commit

---
+++ DragDropViewer/MainForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace DragDropViewer {
+  partial class MainForm {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      this.listFormats = new System.Windows.Forms.ListBox();
+      this.propertyGrid = new System.Windows.Forms.PropertyGrid();
+      this.butFromClipboard = new System.Windows.Forms.Button();
+      this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+      this.tableLayoutPanel1.SuspendLayout();
+      this.flowLayoutPanel1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // tableLayoutPanel1
+      // 
+      this.tableLayoutPanel1.ColumnCount = 2;
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.Controls.Add(this.listFormats, 0, 1);
+      this.tableLayoutPanel1.Controls.Add(this.propertyGrid, 0, 2);
+      this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+      this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+      this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+      this.tableLayoutPanel1.RowCount = 3;
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.Size = new System.Drawing.Size(672, 520);
+      this.tableLayoutPanel1.TabIndex = 0;
+      this.tableLayoutPanel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+      // 
+      // listFormats
+      // 
+      this.listFormats.AllowDrop = true;
+      this.listFormats.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.listFormats.FormattingEnabled = true;
+      this.listFormats.Location = new System.Drawing.Point(3, 38);
+      this.listFormats.Name = "listFormats";
+      this.listFormats.Size = new System.Drawing.Size(330, 236);
+      this.listFormats.TabIndex = 1;
+      this.listFormats.SelectedIndexChanged += new System.EventHandler(this.listFormats_SelectedIndexChanged);
+      this.listFormats.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+      // 
+      // propertyGrid
+      // 
+      this.propertyGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.propertyGrid.LineColor = System.Drawing.SystemColors.ControlDark;
+      this.propertyGrid.Location = new System.Drawing.Point(3, 280);
+      this.propertyGrid.Name = "propertyGrid";
+      this.propertyGrid.Size = new System.Drawing.Size(330, 237);
+      this.propertyGrid.TabIndex = 2;
+      // 
+      // butFromClipboard
+      // 
+      this.butFromClipboard.Location = new System.Drawing.Point(3, 3);
+      this.butFromClipboard.Name = "butFromClipboard";
+      this.butFromClipboard.Size = new System.Drawing.Size(154, 23);
+      this.butFromClipboard.TabIndex = 0;
+      this.butFromClipboard.Text = "Get Clipboard Data";
+      this.butFromClipboard.UseVisualStyleBackColor = true;
+      this.butFromClipboard.Click += new System.EventHandler(this.butFromClipboard_Click);
+      // 
+      // flowLayoutPanel1
+      // 
+      this.flowLayoutPanel1.AutoSize = true;
+      this.tableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel1, 2);
+      this.flowLayoutPanel1.Controls.Add(this.butFromClipboard);
+      this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+      this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+      this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+      this.flowLayoutPanel1.Size = new System.Drawing.Size(666, 29);
+      this.flowLayoutPanel1.TabIndex = 3;
+      this.flowLayoutPanel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+      // 
+      // MainForm
+      // 
+      this.AllowDrop = true;
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(672, 520);
+      this.Controls.Add(this.tableLayoutPanel1);
+      this.Name = "MainForm";
+      this.Text = "Drag Drop Viewer";
+      this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+      this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+      this.tableLayoutPanel1.ResumeLayout(false);
+      this.tableLayoutPanel1.PerformLayout();
+      this.flowLayoutPanel1.ResumeLayout(false);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    private System.Windows.Forms.ListBox listFormats;
+    private System.Windows.Forms.PropertyGrid propertyGrid;
+    private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    private System.Windows.Forms.Button butFromClipboard;
+  }
+}
+
---
+++ DragDropViewer/MainForm.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DragDropViewer {
+  public partial class MainForm : Form {
+    #region Fields
+    private Control preview;
+    #endregion
+
+    #region Properties
+    private Control PreviewControl {
+      get {
+        return preview;
+      }
+      set {
+        var current = PreviewControl;
+        if (current != null) {
+          tableLayoutPanel1.Controls.Remove(current);
+          current.Dispose();
+          current = null;
+        }
+
+        preview = value;
+
+        if (preview != null) {
+          // Some special cases
+          if (preview is TextBoxBase) {
+            ((TextBoxBase)preview).Multiline = true;
+          }
+
+          value.Dock = DockStyle.Fill;
+
+          tableLayoutPanel1.Controls.Add(value, 1, 1);
+          tableLayoutPanel1.SetRowSpan(value, 2);
+        }
+      }
+    }
+
+    private DataFormat SelectedItem {
+      get {
+        return listFormats.SelectedIndex == -1 ? null : (DataFormat)listFormats.SelectedItem;
+      }
+    }
+    #endregion
+
+    #region Constructors
+    public MainForm() {
+      InitializeComponent();
+    }
+    #endregion
+
+    #region Methods
+    private void LoadData(IDataObject data) {
+      listFormats.SelectedIndexChanged -= listFormats_SelectedIndexChanged;
+      listFormats.BeginUpdate();
+
+      listFormats.Items.Clear();
+      if (data != null) {
+        foreach (var fmt in data.GetFormats(true)) {
+          var item = new DataFormat() {
+            Data = data.GetData(fmt),
+            Name = fmt
+          };
+          listFormats.Items.Add(item);
+        }
+      }
+
+      listFormats.EndUpdate();
+      listFormats.SelectedIndex = -1;
+      listFormats.SelectedIndexChanged += listFormats_SelectedIndexChanged;
+
+    }
+
+    private void OnSelectedItemChanged() {
+      var selected = SelectedItem;
+
+      propertyGrid.SelectedObject = selected?.Data;
+
+      if ((selected == null) || (selected.Data == null)) {
+        PreviewControl = null;
+        return;
+      }
+
+      if (selected.Name == "Rich Text Format") {
+        PreviewControl = new RichTextBox() {
+          Rtf = (string)selected.Data
+        };
+
+      } else if (selected.Name == "text/html") {
+        var stream = selected.Data as Stream;
+        stream.Position = 0;
+
+        PreviewControl = new WebBrowser() {
+          DocumentStream = stream
+        };
+
+      } else if (selected.Type == typeof(string)) {
+        PreviewControl = new TextBox() {
+          Text = (string)selected.Data
+        };
+
+      } else if (selected.Type == typeof(string[])) {
+        PreviewControl = new TextBox() {
+          Lines = (string[])selected.Data
+        };
+
+      } else if (selected.Type == typeof(MemoryStream)) {
+        var ms = selected.Data as MemoryStream;
+        ms.Position = 0;
+
+        Encoding enc;
+        if (
+          selected.Name.EndsWith("W")
+          || selected.Name.Contains("x-moz")
+          ) {
+          enc = Encoding.Unicode;
+        } else {
+          enc = Encoding.UTF8;
+        }
+
+        using (var reader = new StreamReader(ms, enc, true, 1024, true)) {
+          PreviewControl = new TextBox() {
+            Text = reader.ReadToEnd()
+          };
+        }
+
+      } else if (selected.Type == typeof(Bitmap)) {
+        PreviewControl = new PictureBox() {
+          Image = (Bitmap)selected.Data,
+          SizeMode = PictureBoxSizeMode.Zoom
+        };
+
+      } else {
+        PreviewControl = null;
+      }
+      
+    }
+    #endregion
+
+    #region Event handlers
+    private void butFromClipboard_Click(object sender, EventArgs e) {
+      LoadData(Clipboard.GetDataObject());
+    }
+
+    private void listFormats_SelectedIndexChanged(object sender, EventArgs e) {
+      OnSelectedItemChanged();
+    }
+
+    private void MainForm_DragEnter(object sender, DragEventArgs e) {
+      e.Effect = e.AllowedEffect;
+    }
+
+    private void MainForm_DragDrop(object sender, DragEventArgs e) {
+      LoadData(e.Data);
+    }
+    #endregion
+
+    #region Inner classes
+    public class DataFormat {
+      public object Data { get; set; }
+
+      public string Name { get; set; }
+
+      public Type Type => Data?.GetType();
+
+      public string TypeName => Type == null ? "null" : Type.Name;
+
+      public override string ToString() {
+        return $"{TypeName} {Name}";
+      }
+    }
+    #endregion
+  }
+}
